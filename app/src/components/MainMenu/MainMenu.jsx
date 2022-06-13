c6f71569a098fa5ae6977040d6789d260061f692
AlexKot <shurik19@ya.ru>
2022-06-13 22:46:52 +0300

    commit menu button welcome1

--- app/src/components/MainMenu/MainMenu.jsx
+++ app/src/components/MainMenu/MainMenu.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Button from '../UI/Button'
 import s from './MainMenu.module.sass'
 
 export default function MainMenu() {
   return (
-    <div className={s.text}>MainMenu</div>
+    <div className={s.main_menu}>
+        <div className={s.logo}>
+            <img src='Vector.png' alt="logo" className={s.menu_logo}/>
+            <p>Pages</p>
+        </div>
+        <div className={s.menu_link}>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Pages</a>
+            <a href="#">Contact Us</a>
+        </div>
+        <Button/>
+    </div>
   )
 }
---
+++ app/src/components/UI/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import s from './Button.module.sass'
+export default function Button() {
+  return (
+    <button className={s.btn}>Order Today</button>
+  )
+}
